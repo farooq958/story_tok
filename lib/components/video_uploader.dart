9ffe2646a6b727715e2aa0ee776ef90303115cfe
hadi-almalki <hadialmalki@guriinteractive.com>
2023-03-01 19:20:26 -0500

    mock up

---
+++ lib/components/video_uploader.dart
@@ -0,0 +1,153 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:storily/components/voice_recording.dart';
+
+enum ImageSourceType { gallery, camera }
+
+class VideoUploader extends StatefulWidget {
+  @override
+  VideoUploaderState createState() => VideoUploaderState();
+}
+
+class VideoUploaderState extends State<VideoUploader> {
+  final ImagePicker _picker = ImagePicker();
+  File? _video;
+  File? _cover;
+
+  void _addVideo(BuildContext context, var type) async {
+    XFile? video = await _picker.pickVideo(source: ImageSource.gallery);
+    setState(() {
+      if (video != null) {
+        _video = File(video!.path);
+      }
+    });
+  }
+
+  void _addCover(BuildContext context, var type) async {
+    XFile? cover = await _picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      if (cover != null) {
+        _cover = File(cover!.path);
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Pick video"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FilledButton(
+                      onPressed: () async {
+                        _addVideo(context, ImageSourceType.gallery);
+                      },
+                      child: Text("Upload video"))
+                ],
+              ),
+
+              // Title
+              Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        child: Text("Title:"),
+                      ),
+                      SizedBox(
+                          width: 200,
+                          child: TextFormField(
+                            maxLines: 1,
+                            decoration: InputDecoration(
+                              border: UnderlineInputBorder(),
+                              hintText: 'Video title',
+                            ),
+                          ))
+                    ],
+                  )),
+
+              // Description
+              Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        child: Text("Description:"),
+                      ),
+                      SizedBox(
+                          width: 200,
+                          child: TextFormField(
+                            maxLines: 1,
+                            decoration: InputDecoration(
+                              border: UnderlineInputBorder(),
+                              hintText: 'Video description',
+                            ),
+                          ))
+                    ],
+                  )),
+
+              // Tags
+              Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(width: 100, child: Text("Tags:")),
+                      SizedBox(
+                          width: 200,
+                          child: TextFormField(
+                            maxLines: 1,
+                            decoration: InputDecoration(
+                              border: UnderlineInputBorder(),
+                              hintText: 'Start typing to add tags',
+                            ),
+                          ))
+                    ],
+                  )),
+
+              // Cover
+              Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(width: 100, child: Text("Cover:")),
+                      IconButton(onPressed: () async {
+                        _addCover(context, ImageSourceType.gallery);
+                      },
+                          icon: Icon(Icons.add_a_photo_rounded)),
+                      SizedBox(
+                          width: 200,
+                          child: _cover != null ? Image.file(
+                            _cover!,
+                            width: 70,
+                            height: 70,
+                          ) : Text(""),
+                          )
+                    ],
+                  )),
+
+
+              // Publish button
+              FilledButton(
+                  onPressed: () {},
+                  child: Text("Publish"))
+            ],
+          ),
+        ));
+  }
+}
