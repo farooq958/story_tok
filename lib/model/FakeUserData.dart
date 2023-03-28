d7bcfa03780bf3f2f6ac2d3297ec15f30e9c4194
NUMBER2\gongp <gongpeipei@gopokyo.com>
2023-03-28 10:32:25 -0300

    Add in fake user ids to use for dev

---
+++ lib/model/FakeUserData.dart
@@ -0,0 +1,8 @@
+class FakeUserData {
+  FakeUserData._();
+  static const String fakeAuthorUserId ="Qc4uMKX1cyeSal2EZ1PKTRfTMx23"; //this person also has a valid author id which links to his author data, and his author data has a user_id that links back to his user data.
+
+  static const String fakeKidUserId = "JmuMU1mkIIFkFuPjU3vP"; //this is a kid user, so he does not have a author_id
+
+
+}
