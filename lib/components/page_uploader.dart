28a63bc8b6b82a5609ac39f3407e147170ca29b1
Jay <jp@savitriya.com>
2023-04-11 20:25:16 -0400

    Implement the background music task.

--- lib/components/page_uploader.dart
+++ lib/components/page_uploader.dart
@@ -332,7 +332,7 @@ class PageUploaderState extends State<PageUploader> {
                             ),
                             InkWell(
                               onTap: () {
-                                if (_images.length < 1) {
+                                if (_images.length < 4) {
                                   Utils().showToastMessage(
                                       "Book has at least 4 pages.", context);
                                 } else {
